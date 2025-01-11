818356556fa3e141a6ec5ead18a5e289d63abb7a
tobias <tobias@tobias-leong.dev>
2025-01-12 02:25:14 +0800

    initial commit

---
+++ programs/counter-solana/src/lib.rs
@@ -0,0 +1,47 @@
+#![allow(clippy::result_large_err)]
+
+use anchor_lang::prelude::*;
+
+declare_id!("4shsUzaFzrvYHJdECyHHHhAuhFNzLabJE3Vx9Cx5VB5t");
+
+#[program]
+pub mod counter_anchor {
+    use super::*;
+
+    pub fn initialize_counter(_ctx: Context<InitializeCounter>) -> Result<()> {
+        msg!("Starting counter");
+        Ok(())
+    }
+
+    pub fn increment(ctx: Context<Increment>) -> Result<()> {
+        ctx.accounts.counter.count = ctx.accounts.counter.count.checked_add(1).unwrap();
+        msg!("Incremented counter to {}", ctx.accounts.counter.count);
+        Ok(())
+    }
+}
+
+#[derive(Accounts)]
+pub struct InitializeCounter<'info> {
+    #[account(mut)]
+    pub payer: Signer<'info>,
+
+    #[account(
+        init,
+        space = 8 + Counter::INIT_SPACE,
+        payer = payer
+    )]
+    pub counter: Account<'info, Counter>,
+    pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+pub struct Increment<'info> {
+    #[account(mut)]
+    pub counter: Account<'info, Counter>,
+}
+
+#[account]
+#[derive(InitSpace)]
+pub struct Counter {
+    count: u64,
+}
